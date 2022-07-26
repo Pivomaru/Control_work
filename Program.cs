2e0ebfec705b8942eba6a2a6be9c242bbc076e26
Evgeniy <spardasenpai@gmail.com>
2022-07-26 17:36:30 +0300

    Add Print

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿void methodArray(string[] array)
 {
-    string[] newarr = new string[array.Length]
+    string[] newarr = new string[array.Length];
      for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
@@ -11,3 +11,18 @@
     }
     Console.WriteLine();
 }
+
+void Print(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)" };
+
+methodArray(array);
+Console.WriteLine();
+Print(array);
