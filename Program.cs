d8e98ddd8549af6adb94a88d911a4bbd4795058d
Evgeniy <spardasenpai@gmail.com>
2022-07-26 17:21:19 +0300

    Start creating prorgramm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void methodArray(string[] array)
+{
+    string[] newarr = new string[array.Length]
+     for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarr[i] = array[i]; ;
+            Console.WriteLine(newarr[i]);
+        }
+    }
+    Console.WriteLine();
+}
